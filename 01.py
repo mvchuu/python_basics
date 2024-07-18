433df73dd7bd789e6e5d7e96e6e481564035d2df
Kamil Machowski <113833517+mvchuu@users.noreply.github.com>
2024-07-18 12:36:18 +0200

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,13 @@
+print(5 + 43)
+print(5 - 43)
+print(5 * 43)
+print(5 / 43)
+print(43 % 5)
+print("Hello world")
+print("I like", "chocolate " * 5)
+
+var1 = "cookies"
+var2 = input("What do you want drink?")
+var3 = " and"
+
+print(var1 + var3 + " " + var2)
