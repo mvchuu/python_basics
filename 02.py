694c09d0b449d2c03c1732f6322160f45bd8fd99
Kamil Machowski <113833517+mvchuu@users.noreply.github.com>
2024-07-18 12:56:42 +0200

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,27 @@
+print("Order: ")
+print(2+2*2)
+print((2+2)*2)
+
+print("Division: ")
+print(5/2)
+print(5//2)
+
+print("Multiplication: ")
+print(2*3)
+print(2**3)
+
+print("Short: ")
+x=4
+print(x)
+x += 1
+print(x)
+
+print("Type conversion: ")
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+print(a+b)
+#print(int(a)+int(b))
+print(float(a)+float(b))
+print(str(a)+str(b))
+
+del a,b
